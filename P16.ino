09434e8e616fc104a322cff02437d6a4562ed301
JuanCManeiro <118885303+JuanCManeiro@users.noreply.github.com>
2022-12-21 10:08:42 +0100

    introduccion dun led multicor indicador do estado
    Introducido o novo led cunha soa resistencia, no lugar de unha por cada led, situada no cátodo

--- P16.ino
+++ P16.ino
@@ -108,3 +108,65 @@ void loop()
     }
   }
 }  
+/* Nesta parte engadín un led multicor para ver na montaxe cal
+é a saía que esta activa dando unha cor a cada unha das etapas, 
+desta maneira seria como se o led que acende o pin 12 fose o motor do rego 
+e o led multicor o que nos indica que tipo de rego se fai e se esta operativo
+*/
+// C++ code
+//
+int A = 0;
+
+int B = 0;
+
+int C = 0;
+
+void setup()//novas saidas 5, 6, 7
+{
+  pinMode(10, INPUT);
+  pinMode(9, INPUT);
+  pinMode(8, INPUT);
+  pinMode(12, OUTPUT);
+  pinMode(5, OUTPUT);
+  Serial.begin(9600);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  A = digitalRead(10);
+  B = digitalRead(9);
+  C = digitalRead(8);
+  if (((!(A == HIGH)) && B == HIGH) && (!(C == HIGH))) {
+    digitalWrite(12, HIGH);
+    digitalWrite(5, HIGH);
+    delay(2000); // Wait for 2000 millisecond(s)
+    Serial.print(" rego 1");
+  } else {
+    if ((A == HIGH && B == HIGH) && (!(C == HIGH))) {
+      digitalWrite(12, HIGH);
+      digitalWrite(6, HIGH);
+      delay(2000); // Wait for 2000 millisecond(s)
+      Serial.print(" rego 2");
+    } else {
+      if ((A == HIGH && B == HIGH) && C == HIGH) {
+        digitalWrite(12, HIGH);
+        digitalWrite(7, HIGH);
+        delay(2000); // Wait for 2000 millisecond(s)
+        Serial.print(" rego 3");
+      } else {// importante dar a orde para que se apague tanto o motor como o led indicador de estado
+        digitalWrite(12, LOW);
+        digitalWrite(5, LOW);
+        digitalWrite(6, LOW);
+        digitalWrite(7, LOW);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(4000); // Wait for 4000 millisecond(s)
+        Serial.println("non fai falla regar");
+      }
+    }
+  }
+}
+/*cambio feito*/
+// agora tentarei introducir un rele na montaxe que active o motor do rego
