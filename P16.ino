2617847d7a9f2982589708648797e90309f5f68a
JuanCManeiro <118885303+JuanCManeiro@users.noreply.github.com>
2022-12-21 09:39:07 +0100

    Actualizacion de texto
    Ultimo texto engadido coa solución do led que non apagaba e explicacion de procesos

--- P16.ino
+++ P16.ino
@@ -47,4 +47,64 @@ void loop()
     }
   }
 }
-  
+/* Vale agora si que detectei o fallo no programa, a miña
+  secuencia facia que cando se activaba o rego o led acendia
+    en calquera das secuencias, pero ainda que cambiase de secuencia 
+      ou que o rego se desactivase o led permanecia aceso xa que ningúnha 
+        orde lle decia que tiña que apagarse*/
+// C++ code
+// 
+   /* definimos os pulsadores como as entradas do sistema A, B e C, 
+por defecto dalles un 0 o estado baixo coa resistencia en formato pull-down 
+  esto continuara asi, en caso de poñer pull-up o pulsador funcionara á inversa
+**************/ 
+int A = 0;
+
+int B = 0;
+
+int C = 0;
+
+void setup()
+  /** marcamos como entradas os pins 10, 9 e 8 e como saídas os pins 12 e o 13
+ que se corresponde co led integrado, tamen definimos o "serial begin" que ven sendo 
+ o monitor en serie donde a cada orde lle dou "voz" para corrovorar que se fai o paso 
+ a traves dela 
+  **/
+{
+  pinMode(10, INPUT);
+  pinMode(9, INPUT);
+  pinMode(8, INPUT);
+  pinMode(12, OUTPUT);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  A = digitalRead(10);
+  B = digitalRead(9);
+  C = digitalRead(8);
+  if (((!(A == HIGH)) && B == HIGH) && (!(C == HIGH))) {
+    digitalWrite(12, HIGH);
+    delay(2000); // Wait for 2000 millisecond(s)
+    Serial.print(" rego 1");
+  } else {
+    if ((A == HIGH && B == HIGH) && (!(C == HIGH))) {
+      digitalWrite(12, HIGH);
+      delay(2000); // Wait for 2000 millisecond(s)
+      Serial.print(" rego 2");
+    } else {
+      if ((A == HIGH && B == HIGH) && C == HIGH) {
+        digitalWrite(12, HIGH);
+        delay(2000); // Wait for 2000 millisecond(s)
+        Serial.print(" rego 3");
+      } else {
+        digitalWrite(12, LOW);/*este foi o cambio que fixo que se apagase o led cando 
+    o rego estivese desactivado*/
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(4000); // Wait for 4000 millisecond(s)
+        Serial.println("non fai falla regar");
+      }
+    }
+  }
+}  
